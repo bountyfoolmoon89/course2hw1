7177d3b3c24a58dd63f84910a57cd17cff87dd57
Егор <bountyfoolmoon89@yandex.com>
2022-12-02 19:59:46 +0300

    part 1, 2, 3 поправки

--- src/Automobile.java
+++ src/Automobile.java
@@ -8,28 +8,29 @@ public class Automobile {
     int year;
     String country;
 
-    Automobile(String brand, String model, int year, String country, String color, double engineVolume){
+    Automobile(String brand, String model, int year, String country, String color, double engineVolume) {
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.country = country;
         this.color = color;
         this.engineVolume = engineVolume;
-        if (year <= 0){
+        if (year <= 0) {
             this.year = 2000;
         } else if (engineVolume <= 0) {
             this.engineVolume = 1.5;
-        } else if (brand == null | Objects.equals(brand, "")) {
+        } else if (brand == null | brand.isEmpty() | brand.isBlank()) {
             this.brand = "default";
-        } else if (model == null | Objects.equals(model, "")) {
+        } else if (model == null | model.isEmpty() | model.isBlank()) {
             this.model = "default";
-        } else if (country == null | Objects.equals(country, "")) {
+        } else if (country == null | country.isEmpty() | country.isBlank()) {
             this.country = "default";
-        } else if (color == null | Objects.equals(color, "")) {
+        } else if (color == null | color.isEmpty() | color.isBlank()) {
             this.color = "белый";
         }
     }
-    public String toString(){
-        return brand+" "+model+", "+year+" года выпуска. Производство: "+country+", "+color+" цвет кузова. Объем двигателя - "+engineVolume+" л.";
+
+    public String toString() {
+        return brand + " " + model + ", " + year + " года выпуска. Производство: " + country + ", " + color + " цвет кузова. Объем двигателя - " + engineVolume + " л.";
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,30 +1,30 @@
-import java.util.Objects;
+import java.time.LocalDate;
 
 public class Person {
-    int dateOfBirth;
-    String name;
+    final int yearOfBirth;
+    final String name;
     String city;
     String jobTitle;
 
 
-    Person(int dateOfBirth, String name, String city, String jobTitle) {
-        this.dateOfBirth = dateOfBirth;
+    Person(int yearOfBirth, String name, String city, String jobTitle) {
+        this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.city = city;
         this.jobTitle = jobTitle;
-        if (dateOfBirth <= 0) {
+        if (yearOfBirth <= 0) {
             System.err.println("Возраст не может быть отрицательным или равным нулю");
-        } else if (name == null | Objects.equals(name, "")) {
-            System.out.println("Привет! Меня зовут " + "пожалуйста, укажите имя" + ". Я из города " + city + ". Я родился в " + dateOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
-        } else if (city == null | Objects.equals(city, "")) {
-            System.out.println("Привет! Меня зовут " + name + ". Я из города " + "пожалуйста, укажите город" + ". Я родился в " + dateOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
-        } else if (jobTitle == null | Objects.equals(jobTitle, "")) {
-            System.out.println("Привет! Меня зовут " + name + ". Я из города " + city + ". Я родился в " + dateOfBirth + " году. Я работаю на должности " + "пожалуйста, укажите должность" + ". Будем знакомы!");
+        } else if (name == null | name.isEmpty() | name.isBlank()) {
+            System.out.println("Привет! Меня зовут " + "пожалуйста, укажите имя" + ". Я из города " + city + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+        } else if (city == null | city.isEmpty() | city.isBlank()) {
+            System.out.println("Привет! Меня зовут " + name + ". Я из города " + "пожалуйста, укажите город" + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+        } else if (jobTitle == null | jobTitle.isEmpty() | jobTitle.isBlank()) {
+            System.out.println("Привет! Меня зовут " + name + ". Я из города " + city + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + "пожалуйста, укажите должность" + ". Будем знакомы!");
         }
     }
 
-        void sayHello(){
-            System.out.println("Привет! Меня зовут "+name+". Я из города "+city+". Я родился в "+dateOfBirth+" году. Я работаю на должности "+jobTitle+". Будем знакомы!");
+    void sayHello() {
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + city + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
 
     }
 }
